1983d6cd8bdeb4e66622b13c34d011f9efbb6056
AvraamNikolaou <avnikolaou@hotmail.com>
2020-12-16 17:07:35 +0200

    code cleanup

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -24,7 +24,7 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable): void {
-        const marker =new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
